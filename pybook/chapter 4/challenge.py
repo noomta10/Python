18958ff56dce980f2d78ebdce69cf2a395fc631f
noam <noomta10@gmail.com>
2021-09-02 18:23:58 +0300

    push

---
+++ pybook/chapter 4/challenge.py
@@ -0,0 +1,15 @@
+import random
+
+def run_casino(low,high,threshold):
+    random_num = random.randint(low, high)
+    if  random_num > threshold:
+        print("YOU WIN")
+    else:
+        print("YOU LOOSE")
+
+low = int(input("enter low number: "))
+high = int(input("enter high number: "))
+threshold = int(input("enter threshold: "))
+run_casino(low, high, threshold)
+run_casino(low, high, threshold)
+run_casino(low, high, threshold)
