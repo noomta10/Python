4b2aaea8b51c331c08229a406272593e47f7a093
noam <noomta10@gmail.com>
2021-09-19 15:55:24 +0300

    push

--- pybook/chapter 6/challenge 3.py
+++ pybook/chapter 6/challenge 3.py
@@ -2,8 +2,15 @@ def main():
     sentence = input("write a sentence: ")
     if "duplicate" in sentence:
         duppos = sentence.find("duplicate")
-        print(sentence)
+        sppos = sentence.find(" ", duppos)
+        num = int(sentence[sppos+1])
+        wordpos = sentence.find(" ", sppos+1)
+        wordend = sentence.find(" ", wordpos+1)
+        word = sentence[wordpos:wordend]
+        print(sentence[:duppos] + word * num + sentence[wordend:])
+
     else:
         print(sentence)
+
 
 main()
