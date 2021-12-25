25e6a6eb6569a25e70b36303840439e501342ed8
noam <noomta10@gmail.com>
2021-12-25 13:39:06 +0200

    push

--- pybook/chapter 10- Tuples/Q3/Q3_parse.py
+++ pybook/chapter 10- Tuples/Q3/Q3_parse.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 
 def main():
     file = open("salary.txt", "r")
@@ -22,27 +24,17 @@ def main():
 
 def calculate_line_hours(line, day_hour):
     line = line.split()
+
     date = line[0]
-    month = date[5:7]
-    day = date[8:10]
+    start_time_string = line[1]
+    end_time_string = line[2]
 
-    start_time = line[1]
-    start_hour_to_minutes = calculate_minutes(start_time)
+    time_format = "%Y_%m_%d %H:%M"
+    start_time = datetime.strptime(date + " " + start_time_string, time_format)
+    end_time = datetime.strptime(date + " " + end_time_string, time_format)
 
-    end_hour = line[2]
-    end_hour_to_minutes = calculate_minutes(end_hour)
-
-    total_minutes = end_hour_to_minutes - start_hour_to_minutes
-    total_hours = total_minutes / 60
-    day_hour[(month, day)] = total_hours
-
-
-def calculate_minutes(time):
-    time_split = time.split(sep=":")
-    time_hour = time_split[0]
-    time_minute = time_split[1]
-    hour_to_min = int(time_hour) * 60 + int(time_minute)
-    return hour_to_min
+    total_hours = (end_time - start_time).total_seconds() / 3600
+    day_hour[(start_time.month, start_time.day)] = total_hours
 
 
 def split_hours(day_hour, normal_hours_count, month_hours, month_extra):
@@ -75,9 +67,10 @@ def calculate_payment(hour_pay, extra_add_percent, month_hours, month_extra):
         payment_normal = month_hours[month] * hour_pay
         payment_extra = month_extra[month] * (1 + extra_add_percent / 100) * hour_pay
         total_payment = payment_extra + payment_normal
+        line = "Month: {}\n Hours normal: {}\n Hours extra: {}\n" \
+               " payment normal: {}\n Payment extra: {}\n Payment total: {}\n\n"
         file.write(
-            "Month: {}\n Hours normal: {}\n Hours extra: {}\n" +
-            " payment normal: {}\n Payment extra: {}\n Payment total: {}\n\n".format(
+            line.format(
                 month, month_hours[month], month_extra[month],
                 payment_normal, payment_extra, total_payment))
 
