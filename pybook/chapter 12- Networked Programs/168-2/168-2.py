42c4afb288c0a5825d2c96d83c34e7aa229a2363
noam <noomta10@gmail.com>
2022-01-30 16:26:36 +0200

    push

--- pybook/chapter 12- Networked Programs/168-2/168-2.py
+++ pybook/chapter 12- Networked Programs/168-2/168-2.py
@@ -6,11 +6,15 @@ mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect((url_divided[2], 80))
 cmd = 'GET {} HTTP/1.0\r\n\r\n'.format(url).encode()
 mysock.send(cmd)
+character_count = 0
 
 while True:
-    data = mysock.recv(512)
+    data = mysock.recv(500)
+    character_count += len(data)
+    if character_count <= 3000:
+        print(data.decode(), end='')
     if len(data) < 1:
         break
-    print(data.decode(), end='')
+print("\nTotal number of characters: {}".format(character_count))
 
 mysock.close()
