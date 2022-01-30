8201f0cc699990f456f5234580c7dfa9e1ee5e64
noam <noomta10@gmail.com>
2022-01-30 18:23:28 +0200

    push

--- pybook/chapter 12- Networked Programs/168-2/168-2.py
+++ pybook/chapter 12- Networked Programs/168-2/168-2.py
@@ -3,16 +3,23 @@ import socket
 url = input("Enter URL: ")
 url_divided = url.split("/")
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect((url_divided[2], 80))
+url_name = url_divided[2]
+mysock.connect((url_name, 80))
 cmd = 'GET {} HTTP/1.0\r\n\r\n'.format(url).encode()
 mysock.send(cmd)
 character_count = 0
+printed = 0
 
 while True:
-    data = mysock.recv(500)
+    data = mysock.recv(1000)
     character_count += len(data)
-    if character_count <= 3000:
-        print(data.decode(), end='')
+    if printed < 3000:
+        if character_count < 3000:
+            print(data)
+            printed += len(data)
+        else:
+            print(data[0:3000-printed])
+            printed = 3000
     if len(data) < 1:
         break
 print("\nTotal number of characters: {}".format(character_count))
